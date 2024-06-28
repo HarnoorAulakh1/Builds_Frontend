39ad191ce9f3f7ce6a3c792f9141bc436ad0bd04
Harnoor Singh Aulakh <harnoorsinghaulakh@Harnoors-MacBook-Air.local>
2024-06-28 20:20:17 +0530

    backend link changed

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
+      // '/api': {
+      //   target: 'http://54.163.26.124:3000/',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
       '/api': {
-        target: 'http://54.163.26.124:3000/',
+        target: 'https://builds-backend-1.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
